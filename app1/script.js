33b5ffb8f37f0da98302bd20bbaf34a18b61b5c1
CristhianSantacruz <crimsant@espol.edu.ec>
2023-09-12 23:44:25 -0500

    first commit

---
+++ app1/script.js
@@ -0,0 +1,11 @@
+
+
+const compro = document.getElementById("compro")
+
+document.getElementById("btn").onclick = function () {
+    compro.innerHTML = "Se ha Comprado el producto"
+}
+
+document.getElementById("btn2").onclick = function () {
+    compro.innerHTML = "Precio 💵20.00"
+}
